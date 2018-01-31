56208cf35ac0bc0b254ea4c735c704dba528c7d3
andersonbui <andersonb@hotmail.es>
2018-01-31 09:56:31 -0500

    union cambios mochilas

--- src/main/mochila/cuadratica/graspBasadoMemoria/Grasp.java
+++ src/main/mochila/cuadratica/graspBasadoMemoria/Grasp.java
@@ -58,24 +58,67 @@ public class Grasp extends AlgoritmoMetaheuristico {
 
     @Override
 
+//    public List<Individuo> ejecutar(Funcion funcion) {
+//        Individuo individuo = null;
+//        Individuo best = null;
+//        int maxLen=0,minLen=0;
+//        List<Individuo> listaRecorrido = new ArrayList();
+//        for (k = 0; k < iteraciones; k++) {
+//            individuo = faseConstruccion(minLen, maxLen, individuo);
+//            individuo = faseBusquedaLocal(individuo);
+//            if (best == null || best.compareTo(individuo) < 0) {
+//                best = individuo;
+//            }
+//            listaRecorrido.add(best);
+//        }
+//        return listaRecorrido;
+//    }   
+    /**
+     * GRASP CON REINICIOS BASADO EN LA MEMORIA
+     * 
+     * numLanda es el numero de reinicios usados para intentar mejorar bestLB
+     * numDelta es el numero de iteraciones GRASp
+     * 
+     */
     public List<Individuo> ejecutar(Funcion funcion) {
-        Individuo individuo = null;
-        Individuo best = null;
+        Individuo LB = null;
+        Individuo S = null;
+        Individuo bestLB = null;
+        Individuo solParcialS = null;
+        int numLanda=10;
+        int cont = 0, m = 0;
+        int num_y = 0, num_b = 0;
+        int minLen = num_y;
+        int maxLen = num_y + num_b;
         List<Individuo> listaRecorrido = new ArrayList();
-        for (k = 0; k < iteraciones; k++) {
-//            individuo = faseConstruccion();
-            individuo = faseBusquedaLocal(individuo);
-            if (best == null || best.compareTo(individuo) < 0) {
-                best = individuo;
+        while (cont != numLanda) {
+            for (k = 0; k < iteraciones; k++) {
+                S = faseConstruccion(minLen, maxLen, solParcialS);
+                LB = faseBusquedaLocal(S);
+                if (bestLB == null || bestLB.compareTo(LB) < 0) {
+                    bestLB = LB;
+                }
             }
-            listaRecorrido.add(best);
+            if (bestLB.compareTo(LB) < 0) {
+                bestLB=LB;
+                cont=0;
+            } else {
+                cont++;
+                minLen=maxLen + 1;
+                maxLen = maxLen + num_b;
+            }
+            listaRecorrido.add(bestLB);
         }
         return listaRecorrido;
     }
 
+    /**
+     * Clase interna ItemCalidad que cntiene la informacin del Indice y la
+     * Calidad de un individuo especifico
+     */
     private class ItemCalidad implements Comparable<ItemCalidad> {
 
-        private int indice;
+        private final int indice;
         private Double calidad;
 
         public ItemCalidad(int indice, double calidad) {
@@ -125,17 +168,34 @@ public class Grasp extends AlgoritmoMetaheuristico {
             //linea 9-11:
             double[] qkl = probabilidadSeleccionLRC(listaLRC, Qkl);
             //linea 12:
-
-//            [--,---,-----]=1
-//            [0-0.2,0.2-0.5,0.5-1],t=1,0..,2(|LRC|)
-//            0.45
+            s.set(seleccionItemDeLRC(Qkl, qkl, listaLRC).indice, 1);
+            //linea 13:
+            l++;
         }
-
-        return null;
+        return s;
     }
 
-    private Individuo faseBusquedaLocal(Individuo individuo) {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+    private Individuo faseBusquedaLocal(Individuo s) {
+        double solucionS = funcionGreedy.evaluar(s), solucionShift = 0, solucionSwap = 0;
+        boolean termina = false;
+        while (termina != false) {
+            shift(s);
+            solucionShift = funcionGreedy.evaluar(shift(s));
+            swap(s);
+            solucionSwap = funcionGreedy.evaluar(swap(s));
+            if (solucionShift > solucionS) {
+                if (solucionShift > solucionSwap) {
+                    s = shift(s);
+                }
+            } else if (solucionSwap > solucionS) {
+                if (solucionSwap > solucionS) {
+                    s = swap(s);
+                }
+            } else {
+                termina = true;
+            }
+        }
+        return s;
     }
 
     private List<ItemCalidad> obtenerLRC(List<ItemCalidad> listItemNoSeleccionados, int len) {
@@ -155,8 +215,17 @@ public class Grasp extends AlgoritmoMetaheuristico {
                 itemsNoSeleccionado.add(i);
             }
         }
-
         return itemsNoSeleccionado;
+    }
+
+    private List<Integer> obtenerItemSelecionado(Individuo individuo) {
+        List<Integer> itemsSeleccionado = new ArrayList();
+        for (int i = 0; i < individuo.getDimension(); i++) {
+            if (individuo.get(i) == 1) {
+                itemsSeleccionado.add(i);
+            }
+        }
+        return itemsSeleccionado;
     }
 
     private void sumarAOptimosLocales(Individuo individuo) {
@@ -196,9 +265,66 @@ public class Grasp extends AlgoritmoMetaheuristico {
         return qkl;
     }
 
-    private double seleccionItemLRC() {
+    private ItemCalidad seleccionItemDeLRC(int Qkl, double[] qkl, List<ItemCalidad> listaLRC) {
+        int m;
+        double prob, sum_qkl = 0;
+        m = 1 + Aleatorio.nextInt(Qkl - 1);
+        prob = m / (double) Qkl;
+        for (int i = 0; i < qkl.length; i++) {
+            sum_qkl += qkl[i];
+            if (sum_qkl < prob) {
+                return listaLRC.get(i);
+            }
+        }
+        return null;
+    }
 
-        return 0.;
+    /**
+     * El movimient Shift adicina un elemento no seleccionado a la solución
+     * actual o remueve un elemento seleccionado de esta.
+     *
+     * @param individuo
+     * @param listItemNoSeleccionados
+     * @return
+     */
+    private Individuo shift(Individuo individuo) {
+        int aleatorio = 0, maxLen = individuo.getDimension();
+        aleatorio = Aleatorio.nextInt(maxLen);
+        if (individuo.get(aleatorio) == 1) {
+            individuo.set(aleatorio, 0);
+        } else {
+            individuo.set(aleatorio, 1);
+        }
+
+        return individuo;
+    }
+
+    /**
+     * El movimiento Swap reemplaza un elemento seleccionado por uno no
+     * seleccionado
+     *
+     * @param individuo
+     * @param listItemNoSeleccionados
+     * @return
+     */
+    private Individuo swap(Individuo individuo) {
+
+        int maxLen = individuo.getDimension();
+        int aleatorio = Aleatorio.nextInt(maxLen);
+        List<Integer> listaItemNoSelect = new ArrayList();
+        for (int i = 0; i < individuo.getDimension(); i++) {
+            if (individuo.get(i) == 0) {
+                listaItemNoSelect.add(i);
+            }
+        }
+        int maxLenNS = listaItemNoSelect.size();
+        int aleatrioNS = Aleatorio.nextInt(maxLenNS);
+        if (individuo.get(aleatorio) == 1) {
+            individuo.set(aleatorio, listaItemNoSelect.get(aleatrioNS));
+        }
+//            lisNS[0,1,3]-> 1
+//            [0,0,1,0,1]->2
+        return individuo;
     }
 
 }
