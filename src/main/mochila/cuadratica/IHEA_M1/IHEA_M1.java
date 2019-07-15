d04d3e3e2c746a240354f3b57fc4b9f025804e30
andersonbui <andersonb@hotmail.es>
2019-07-14 22:25:56 -0500

    empezando a separar mejoras

--- src/main/mochila/cuadratica/IHEA_M1/IHEA_M1.java
+++ src/main/mochila/cuadratica/IHEA_M1/IHEA_M1.java
@@ -14,7 +14,7 @@
  * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
-package main.mochila.cuadratica.hyperplane_exploration_ajustado;
+package main.mochila.cuadratica.IHEA_M1;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -25,91 +25,15 @@ import static main.mochila.cuadratica.utilidades.UtilCuadratica.swap;
  *
  * @author debian
  */
-public class IteratedHyperplaneExplorationAlgoritm_A extends IteratedHyperplaneExplorationAlgoritm {
+public class IHEA_M1 extends IteratedHyperplaneExplorationAlgoritm {
 
-    protected int ub; // lower bown
-
-    protected boolean saltar;
-
-    public IteratedHyperplaneExplorationAlgoritm_A(FuncionMochilaIHEA funcion) {
+    public IHEA_M1(FuncionMochilaIHEA funcion) {
         super(funcion);
         ub = funcion.obtenerUpperBound();
-        saltar = false;
         L = 20;
     }
 
-    public boolean isSaltar() {
-        return saltar;
-    }
-
-    public void setSaltar(boolean saltar) {
-        this.saltar = saltar;
-    }
-
-    /*Aplica la estructura de vecindario h a la s_inicial, un numero de intentos 
-    determinado (intentosEncontrarMejor) y va comparando si s_inicial mejora*/
-    protected IndividuoIHEA encontrarMejor(IndividuoIHEA s_inicial, int h) {
-        IndividuoIHEA aux;
-        s_inicial = s_inicial.clone();
-        boolean mejoro = false;
-        int contador = 3;
-        do {
-            if (h == 1) {
-                aux = (IndividuoIHEA) swap(s_inicial);
-            } else {
-                aux = (IndividuoIHEA) add_factible(s_inicial);
-            }
-            if (aux != null) {
-                mejoro = s_inicial.compareTo(aux) < 0;
-                if (mejoro) {
-                    s_inicial = aux;
-                    break;
-                }
-            }
-        } while (contador-- >= 0);
-        return s_inicial;
-    }
-
-    /**
-     * realiza un mejoramiento al individuo utilizando addElemento y swap. tipo
-     * vns
-     *
-     * @param original
-     * @return
-     */
-    @Override
-    protected IndividuoIHEA descent(IndividuoIHEA original) { ////////////OPCION DE MEJORAR EN TIEMPO (for)
-        //Variable para el tipo de estructura de vecindario
-        int h = 1;
-        IndividuoIHEA s_inicial = original.clone();
-        IndividuoIHEA solEncontrada;
-        while (h <= 2) {
-            //Aplica una estructura de vecindario h a la s_inicial para mejorar
-            solEncontrada = encontrarMejor(s_inicial, h);
-            /*Si mejora s_inicial permanece en h1 (si h=1) o se devuelve a h1 encaso que este en h2 (h=2),
-            valida que no sea un optimo local(osea que exista una mejor solucion)*/
-            if (solEncontrada.compareTo(s_inicial) > 0) {
-                s_inicial = solEncontrada;
-                h = 1;
-            } else {
-                h++;
-            }
-        }
-        return s_inicial;
-    }
-
-    @Override
     protected IndividuoIHEA tabuSearchEngine(int L, IndividuoIHEA x_inicial, IndividuoIHEA x_referencia) {
-//        double[] vecValPesos = parametros.getVectorPesos();
-//        double sumaPesos = UtilCuadratica.suma(vecValPesos);
-//        double div_sp_c = sumaPesos / parametros.getCapacidad();// (suma pesos) / capacidad
-//        if (div_sp_c <= 1.0 ) {
-//            return super.tabuSearchEngine(L, x_inicial, x_referencia);
-//        }
-        return tabuSearchEngineAux(L, x_inicial, x_referencia);
-    }
-
-    protected IndividuoIHEA tabuSearchEngineAux(int L, IndividuoIHEA x_inicial, IndividuoIHEA x_referencia) {
 
         // almacenamiento de valores tabu
         int[][] tabu;
@@ -156,7 +80,6 @@ public class IteratedHyperplaneExplorationAlgoritm_A extends IteratedHyperplaneE
             for (int j : I1) {
                 viol_capacidad = funcion.getCapacidad() - x.pesar() + funcion.peso(j);
                 calidadi = x.getCalidad() - funcion.contribucion(j, x);
-                saltar = false;
                 for (int i : I0) {
                     // linea 11:
                     // linea 12:
